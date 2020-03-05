3221f568164e263742c78d1134e2677b567e5e19
chotow <i@choyri.com>
2020-03-05 22:04:49 +0800

    feat(post): 自动增加 more
    用于实现「继续阅读」

--- functions.php
+++ functions.php
@@ -16,3 +16,58 @@ function thePasswordFormFilter(string $output): string
 }
 
 add_filter('the_password_form', 'thePasswordFormFilter');
+
+function autoAddMoreForPost(WP_Post &$post)
+{
+    if (is_admin()) {
+        return;
+    }
+
+    $post->post_content = trim($post->post_content);
+
+    // 分割段落
+    preg_match_all('/\n+\s+/', $post->post_content, $eolMatches, PREG_OFFSET_CAPTURE);
+
+    $getPos = function (array $matches, int $paragraph = 2): int {
+        $wantDirectReturn = false;
+
+        while (true) {
+            if (!isset($matches[0][$paragraph - 1])) {
+                return 0;
+            }
+
+            // 因为用了 PREG_OFFSET_CAPTURE，多了一个 [1] 用于表示 pos
+            $pos = $matches[0][$paragraph - 1][1];
+
+            if ($wantDirectReturn) {
+                return $pos;
+            }
+
+            // 如果长度不够 300，继续往下一段落探索
+            if ($pos < 300) {
+                $paragraph++;
+                continue;
+            }
+
+            // 如果长度大于 1000，太长了，返回上一段落
+            if ($pos > 1000) {
+                $paragraph--;
+                $wantDirectReturn = true;
+                continue;
+            }
+
+            // 长度还可以，返回吧
+            return $pos;
+        }
+
+        return 0;
+    };
+
+
+    if ($pos = $getPos($eolMatches)) {
+        $replacement = PHP_EOL . '<!--more-->' . PHP_EOL;
+        $post->post_content = substr_replace($post->post_content, $replacement, $pos, 0);
+    }
+}
+
+add_action('the_post', 'autoAddMoreForPost');
